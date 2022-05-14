316311b495ea50580ca8a6d88663fabc45f12a7c
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2022-05-14 10:57:45 -0500

    add apollo files

--- src/components/SingleForm.tsx
+++ src/components/SingleForm.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useMutation, gql } from "@apollo/client"
-// import GravityForm  from "wpgravitybundle"
+import GravityForm, { useGravityFormMutation } from "wpgravitybundle"
 
 interface SingleFormProps {
   form: any
@@ -9,37 +9,37 @@ interface SingleFormProps {
 const SingleForm: React.FC<SingleFormProps> = ({ form }) => {
   const buttonClass = "mt-4 sm:mt-0 sm:ml-3"
 
-//   const gravityFormMutation = useGravityFormMutation(form)
-//   const SUBMIT_FORM = gql`
-//     ${gravityFormMutation}
-//   `
-//   const [submitForm, { data, loading, error }] = useMutation(SUBMIT_FORM)
+  const gravityFormMutation = useGravityFormMutation(form)
+  const SUBMIT_FORM = gql`
+    ${gravityFormMutation}
+  `
+  const [submitForm, { data, loading, error }] = useMutation(SUBMIT_FORM)
 
   const handleSubmit = (values: any) => {
-    // return submitForm({ variables: values })
+    return submitForm({ variables: values })
   }
 
-//   if (loading) {
-//     return <p>Loading... </p>
-//   }
+  if (loading) {
+    return <p>Loading... </p>
+  }
 
-//   if (error) {
-//     return <p>There was an error submitting the form: {error.message}</p>
-//   }
+  if (error) {
+    return <p>There was an error submitting the form: {error.message}</p>
+  }
 
-//   if (data) {
-//     return <p>Confirmation message here!</p>
-//   }
+  if (data) {
+    return <p>Confirmation message here!</p>
+  }
 
   return (
     <>
       {form && (
         <>
-          {/* <GravityForm
+          <GravityForm
             form={form}
             buttonClass={buttonClass}
             onSubmit={handleSubmit}
-          /> */}
+          />
         </>
       )}
     </>
